ec1741a522f17b86e1b2d33b66d142cf9b22604a
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-07-28 21:19:10 +0100

    add cloudinary

--- components/datas/clock.jsx
+++ components/datas/clock.jsx
@@ -1,9 +1,7 @@
 import useEffect from 'react'
 
 export default function Clock() {
-
-
-
+  
   return (
     <div className="mt-0 flex justify-end space-x-1 text-white text-lg sm:space-x-0 sm:text-sm sm:font-light">
       <p>{hour}</p>
@@ -16,6 +14,7 @@ export default function Clock() {
 }
 
 let myDate = new Date();
+
 let hour = myDate.getHours();
 let min = myDate.getMinutes();
 let seg = myDate.getSeconds();
