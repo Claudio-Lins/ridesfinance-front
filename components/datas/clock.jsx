b516bff5704dbfff512da3929c56dd0091fd6d12
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-07-30 19:09:24 +0100

    Update clock.jsx

--- components/datas/clock.jsx
+++ components/datas/clock.jsx
@@ -2,6 +2,7 @@ import useEffect from 'react'
 
 export default function Clock() {
   
+  
   return (
     <div className="mt-0 flex justify-end space-x-1 text-white text-lg sm:space-x-0 sm:text-sm sm:font-light">
       <p>{hour}</p>
@@ -18,3 +19,4 @@ let myDate = new Date();
 let hour = myDate.getHours();
 let min = myDate.getMinutes();
 let seg = myDate.getSeconds();
+
