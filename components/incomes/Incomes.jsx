9acd25fa7d0e920d42efb8084d985fc6566722c5
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-08-04 10:51:36 +0100

    Update Incomes.jsx

--- components/incomes/Incomes.jsx
+++ components/incomes/Incomes.jsx
@@ -4,7 +4,7 @@ export default function Incomes(){
     return(
         <>
             <div className="flex justify-evenly">
-                <IncomeBox id="incomeToday"  title="Today" amount='199,00 €'/>
+                <IncomeBox id="incomeToday"  title="Today" amount='499,00 €'/>
                 <IncomeBox id="incomeweek" title="Week" amount='900,00 €'/>
                 <IncomeBox id="incomeMonth" title="Month" amount='3.999,00 €'/>
             </div>
